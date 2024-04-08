4c19b358e3696f99aaba0b7a05a3d1f583ed4862
Mich-Tapawan <mik.tapawan@gmail.com>
2024-04-08 21:18:02 +0800

    Added size field

--- itemsDB.sql
+++ itemsDB.sql
@@ -1,4 +1,5 @@
-CREATE TABLE items (
+CREATE TABLE orders (
+    customer_id VARCHAR(50) NOT NULL PRIMARY KEY,
 	item VARCHAR(20) NOT NULL,
     price DECIMAL(4, 2) NOT NULL,
     quantity INT NOT NULL
