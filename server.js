e1eed435ea6ecb052a6a9d5c95bec83d1092cf50
Mich-Tapawan <mik.tapawan@gmail.com>
2024-04-08 22:11:54 +0800

    Made HTTP request post to query the order to the database

--- server.js
+++ server.js
@@ -18,5 +18,22 @@ db.connect((err)=>{
 });
 
 app.post('/insert_data', (req, res)=>{
+    const {customerID, item, price, quantity, size} = req.body;
 
-})
+    const sql = 'INSERT INTO orders (customer_id, item, price, quantity, size) VALUES (?, ?, ?, ?, ?)';
+    const values = [customerID, item, price, quantity, size];
+
+    db.query(sql, values, (err, result)=>{
+        if(err){
+            console.log('Error inserting data:'+err);
+            res.status(500).send('Error inserting data');
+        }
+        else{
+            console.log('Data inserted successfully');
+            res.send('Data inserted successfully');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`));
