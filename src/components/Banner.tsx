218ff3b883a538b5fd4cfcb72b43e7b9bee7ffbc
Mich-Tapawan <mik.tapawan@gmail.com>
2024-10-23 22:31:17 +0800

    Fixed parallax layer positioning

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+export default function Banner() {
+  return (
+    <div className="banner w-full py-5 px-5 md:px-20 lg:px-36">
+      <h2 className="hours text-2xl">OPEN HOURS</h2>
+      <span className="text-8xl font-bold">5AM - 11PM</span>
+      <p className="text-lg">
+        Comfy Café <br />
+        1234 Serene Street, Barangay Maligaya, <br />
+        Ermita, Manila, 1000, Philippines
+      </p>
+      <span className=" font-bold text-2xl">Mon Tue Wed Thu Fri Sat</span>
+    </div>
+  );
+}
--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,11 +1,12 @@
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
     <div className="sec1 w-full h-svh">
-      <Parallax pages={2}>
-        <ParallaxLayer offset={0}>
-          <div className="flex flex-col text-white text-center items-center">
+      <Parallax pages={3}>
+        <ParallaxLayer>
+          <div className="flex flex-col text-white text-center items-center mt-44">
             <span className="mb-4 text-lg">START YOUR DAY WITH ONLY THE</span>
             <h1 className="main-h1 font-bold text-5xl mb-6">BEST COFFEE</h1>
             <p className="w-3/6 leading-loose">
@@ -15,6 +16,9 @@ export default function Home() {
               relax, and let us fuel your morning with the finest flavors.
             </p>
           </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1}>
+          <Banner />
         </ParallaxLayer>
       </Parallax>
     </div>
