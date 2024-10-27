35d070e3354cd8a989a04da38708f19452b3a8cc
Mich-Tapawan <mik.tapawan@gmail.com>
2024-10-27 23:45:23 +0800

    Finished adding content for contact page

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,10 +1,26 @@
 export default function ContactForm() {
   return (
-    <form className="">
-      <input type="text" placeholder="Name" className="" />
-      <input type="email" placeholder="Email" className="" />
-      <textarea placeholder="Leave us a message..." className=""></textarea>
-      <button type="submit" className="bg-primary text-white"></button>
+    <form className="bg-white w-full p-5 flex flex-col items-center gap-5 rounded-3xl">
+      <input
+        type="text"
+        placeholder="Name"
+        className="w-full border border-gray-400 h-14 pl-4 rounded-xl
+      "
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        className="w-full border border-gray-400 h-14 pl-4 rounded-xl
+      "
+      />
+      <textarea
+        placeholder="Leave us a message..."
+        className="w-full border border-gray-400 h-60 pl-4 pt-4 rounded-xl
+        "
+      ></textarea>
+      <button type="submit" className="bg-primary text-white py-4 w-2/3">
+        Submit
+      </button>
     </form>
   );
 }
--- src/layouts/Contact.tsx
+++ src/layouts/Contact.tsx
@@ -1,19 +1,52 @@
 import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer";
+
+type ContactDetail = {
+  img: string;
+  alt?: string;
+  text: string;
+};
+
+const details: ContactDetail[] = [
+  { img: "/assets/mail.png", alt: "email", text: "comfycafe@gmail.com" },
+  { img: "/assets/phone.png", alt: "telephone", text: "(043) 8123 - 4567" },
+  { img: "/assets/mobile.png", alt: "mobile", text: "+63 918 695 0102" },
+];
 
 export default function Contact() {
   return (
-    <div>
-      <div>
-        <h1>GET IN TOUCH</h1>
-        <p>
-          We’d love to hear from you! Whether you’re sharing feedback, have a
-          question, or just want to chat over your favorite coffee blend, Comfy
-          Café is here to connect. We look forward to hearing from you!
-        </p>
+    <>
+      <div className="contact h-fit lg:h-svh w-full text-white px-5 md:px-20 lg:px-36 pt-32 pb-10 lg:pt-44 lg:pb-0 flex flex-col lg:flex-row">
+        <div className="  lg:w-4/6">
+          <h1 className="text-4xl text-center font-bold mb-8 lg:text-6xl lg:text-start ">
+            GET IN TOUCH
+          </h1>
+          <p className="text-center font-thin  mb-10 lg:w-4/6 lg:leading-10  lg:text-xl lg:text-start">
+            We’d love to hear from you! Whether you’re sharing feedback, have a
+            question, or just want to chat over your favorite coffee blend,
+            Comfy Café is here to connect. We look forward to hearing from you!
+          </p>
+          <ul className="flex flex-col gap-7 items-center mb-10 lg:items-start lg:mb-0">
+            {details.map(({ img, alt, text }, index) => (
+              <li key={index} className="w-fit flex items-center gap-5">
+                <div className="w-10">
+                  <img src={img} alt={alt} className="object-cover w-full" />
+                </div>
+                <span
+                  className="md:text-lg lg:text-xl"
+                  style={{ fontFamily: "Poppins, serif" }}
+                >
+                  {text}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="lg:w-3/6">
+          <ContactForm />
+        </div>
       </div>
-      <div>
-        <ContactForm />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
