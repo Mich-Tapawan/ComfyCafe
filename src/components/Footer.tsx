31748c5f8c837b9ffd4344d4840648ff8e4e0138
Mich-Tapawan <mik.tapawan@gmail.com>
2024-10-27 16:49:09 +0800

    Fixed positioning and grid columns at various sizes.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import SocialIcons from "./SocialIcons";
 
 export default function Footer() {
   return (
-    <footer className="bg-primary py-20 px-5 md:px-20 lg:px-36 text-white grid lg:grid-cols-3">
-      <div className="border">
+    <footer className="bg-primary lg:py-20 h-fit px-5 md:px-20 lg:px-36 text-white grid grid-cols-1 lg:grid-cols-6">
+      <div className="border flex flex-col items-center text-center lg:items-start lg:text-start lg:col-span-3 gap-2">
         <SocialIcons custom="ftr-icons" />
         <p className="font-extralight">
           Privacy Notice | Terms of Use | Privacy | CA Supply Chain Act |
@@ -13,7 +13,7 @@ export default function Footer() {
           @2023 Comfy Café Company. All rights reserved.
         </span>
       </div>
-      <div className="ftr-list flex justify-center my-10 lg:my-0 border">
+      <div className="ftr-list flex justify-center my-10 lg:my-0 border col-span-2">
         <div>
           <h5 className="font-bold">Get in touch</h5>
           <ul className="leading-8">
@@ -53,13 +53,15 @@ export default function Footer() {
           </ul>
         </div>
       </div>
-      <div className="w-28 border">
-        <div className="w-24  justify-self-center mb-1">
-          <img src="/logo.png" alt="comfy café" className="w-full" />
+      <div className="col-span-1 border flex flex-col items-center lg:items-end lg:justify-end">
+        <div className="w-28">
+          <div className="w-24  justify-self-center mb-1">
+            <img src="/logo.png" alt="comfy café" className="w-full" />
+          </div>
+          <p className="text-center" style={{ width: "100%" }}>
+            Comfy Café
+          </p>
         </div>
-        <p className="text-center" style={{ width: "100%" }}>
-          Comfy Café
-        </p>
       </div>
     </footer>
   );
