6b148ef97e3382adae00caad8e6b12abae32f44f
Mich-Tapawan <mik.tapawan@gmail.com>
2024-10-29 23:17:45 +0800

    Fixed script for npm run dev for automatic compilation of typescript to the server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -11,13 +9,13 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/test", (req, res) => {
-  res.json({
-    sale: ["black", "affogato", "mocha", "choco"],
-    limited: ["black", "affogato", "mocha", "choco"],
-    best: ["black", "affogato", "mocha", "choco"],
-  });
-  console.log("Test request success");
+    res.json({
+        sale: ["black", "affogato", "mocha", "choco"],
+        limited: ["black", "affogato", "mocha", "choco"],
+        best: ["black", "affogato", "mocha", "choco"],
+    });
+    console.log("Test request success");
 });
 app.listen(3000, () => {
-  console.log("Server running at PORT 3000");
+    console.log("Server running at PORT 3000");
 });
