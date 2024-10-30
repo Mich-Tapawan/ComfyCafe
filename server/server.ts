8169503ff935573bccf50d56c1ab71385458357b
Mich-Tapawan <mik.tapawan@gmail.com>
2024-10-30 18:12:30 +0800

    Created connection between backend to database for getting onsale, limited, best, and available items lists

--- server/server.ts
+++ server/server.ts
@@ -1,35 +1,62 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/getItems", (req: Request, res: Response) => {
-  res.json({
-    sale: ["black", "affogato", "mocha", "choco"],
-    limited: ["black", "affogato", "mocha", "choco"],
-    best: ["black", "affogato", "mocha", "choco"],
-    allItems: [
-      "black",
-      "affogato",
-      "mocha",
-      "choco",
-      "black",
-      "affogato",
-      "mocha",
-      "choco",
-      "black",
-      "affogato",
-      "mocha",
-      "choco",
-      "black",
-      "affogato",
-      "mocha",
-      "choco",
-    ],
-  });
-  console.log("Items request successful");
+const uri =
+  "mongodb+srv://root:BP9Y7Jka77b20XTn@cluster0.z6r3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.log("Error connecting to mongoDB");
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/getItems", async (req: Request, res: Response) => {
+  try {
+    await client.connect();
+
+    const database = client.db("ComfyCafe");
+    const collection = database.collection("products");
+
+    const allItems = await collection.find({ availability: "true" }).toArray();
+    const sale = allItems.filter((item) => item.specialty === "sale");
+    const limited = allItems.filter((item) => item.specialty === "limited");
+    const best = allItems.filter((item) => item.specialty === "best");
+
+    res.status(200).json({
+      sale: sale,
+      limited: limited,
+      best: best,
+      allItems: allItems,
+    });
+    console.log("getItems request success");
+  } catch (error) {
+    console.log("Error fetching data: " + error);
+    res.status(500).json({ message: "Error fetching data" });
+  }
 });
 
 app.listen(3000, () => {
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import ProductItem from "./ProductItem";
 import { MenuContext } from "../contexts/MenuContext";
 
